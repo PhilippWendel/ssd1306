62136867d566b50af814e3a395cb75a16e5ea44d
Philipp Wendel <philippwendel@protonmail.com>
2023-05-30 19:19:31 +0200

    Updated demo

--- src/main.zig
+++ src/main.zig
@@ -26,11 +26,11 @@ pub fn main() !void {
     try ssd1306.clearScreen(true);
     busyloop(1_000_000);
     try ssd1306.clearScreen(false);
-    for (0..8) |lines| {
-        for (0..128) |cols| {
-            try ssd1306.wt.writeAll(&[_]u8{ 0x40, if (lines == 0 or lines == 7 or cols <= 7 or cols >= 120) 0xFF else 0x00 });
-        }
-    }
+    // for (0..8) |lines| {
+    //     for (0..128) |cols| {
+    //         try ssd1306.wt.writeAll(&[_]u8{ 0x40, if (lines == 0 or cols <= 7) 0xFF else 0x00 }); // or lines == 7  or cols >= 120
+    //     }
+    // }
 
     try uart.writer().writeAll("Write Zig-Logo\r\n");
     try ssd1306.setMemoryAddressingMode(.horizontal);
@@ -38,7 +38,6 @@ pub fn main() !void {
     try ssd1306.setPageAddress(1, 6);
     const zig_logo = @embedFile("zig_logo.pbm");
     const zig_img = zig_logo[9..]; // Ignore pbm metadata
-
     // We write in 8x8 bit blocks since our image goes from left to right and then top to bottom,
     // but the display goes 8 bits down and then left to right and then top to bottom,
     for (0..48 / 8) |line| {
@@ -55,24 +54,38 @@ pub fn main() !void {
             }
         }
     }
+    busyloop(2_000_000);
+
+    try ssd1306.setNormalOrInverseDisplay(.inverse);
+    busyloop(2_000_000);
+
+    try ssd1306.setNormalOrInverseDisplay(.normal);
+    busyloop(2_000_000);
+
+    try ssd1306.setContrast(1);
+    busyloop(2_000_000);
+
+    try ssd1306.setContrast(255);
+    busyloop(2_000_000);
+
+    try ssd1306.setCOMOuputScanDirection(false); // Flip up/down
+    busyloop(2_000_000);
+
+    try ssd1306.setCOMOuputScanDirection(true); // Flip up/down
+    busyloop(2_000_000);
+
     try uart.writer().writeAll("Setup scrolling\r\n");
     try ssd1306.deactivateScroll();
-    try ssd1306.continuousHorizontalScrollSetup(.right, 1, 6, 0b111);
     try ssd1306.setVerticalScrollArea(8, 48);
+    try ssd1306.continuousHorizontalScrollSetup(.left, 1, 6, 0b111);
     try ssd1306.activateScroll();
+    busyloop(2_000_000);
 
-    // try ssd1306.entireDisplayOn(.resumeToRam);
-    try ssd1306.setMemoryAddressingMode(.horizontal);
-    try ssd1306.setColumnAddress(0, 127);
-    try ssd1306.setPageAddress(0, 7);
-    var contrast: u8 = 255;
-    try uart.writer().writeAll("Loop\r\n");
-    while (true) {
-        try ssd1306.setContrast(contrast);
-        contrast = if (contrast == 255) 128 else 255;
-        // try ssd1306.wt.writeAll(&[_]u8{ 0x40, 0x00 });
-        busyloop(1_000_000);
-    }
+    try ssd1306.deactivateScroll();
+    try ssd1306.continuousHorizontalScrollSetup(.right, 1, 6, 0b111);
+    try ssd1306.activateScroll();
+    busyloop(2_000_000);
+    while (true) {}
 }
 
 fn busyloop(limit: u24) void {
